85ef7402d6500e841021d9790b8370e8641e6d40
hichamcc <gaddariowen@gmail.com>
2025-06-24 11:24:02 +0100

    admin seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin321@'),
+            'role' => 'admin',
+        ]);
+    
+        //
+    }
+}
